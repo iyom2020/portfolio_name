07553208d44a8fab5ba97851665bcb0006be629c
iyom2020 <sosk020321@gmail.com>
2023-03-31 17:47:01 +0900

    タグの表示を改善

--- lib/component/tag/tag_view.dart
+++ lib/component/tag/tag_view.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 
 Widget TagView(String tagName, Color? tagColor) {
   Color _tagColor = (tagColor != null) ? tagColor : Colors.indigo;
-  double widthNum = 14 * (tagName.length) + 20;
   return Container(
     padding: EdgeInsets.only(top: 5, bottom: 5, left: 7, right: 7),
-    width: widthNum,
     decoration: BoxDecoration(
       color: _tagColor,
       borderRadius: BorderRadius.circular(5),
     ),
-    child: Center(
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(5,0,5,0),
       child: Text(
         tagName,
         style: TextStyle(
